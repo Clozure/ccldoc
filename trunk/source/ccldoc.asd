f7f653d894bd211f23bf407821fb7ea3c228b87c
rme <rme@90ce9a8c-d672-4643-9b85-e16b3cea388b>
2014-10-13 23:44:19 +0000

    New files.

---
+++ trunk/source/ccldoc.asd
@@ -0,0 +1,27 @@
+;;;   Copyright (C) 2014 Clozure Associates
+;;;   This file is part of Clozure CL.  
+;;;
+;;;   Clozure CL is licensed under the terms of the Lisp Lesser GNU Public
+;;;   License , known as the LLGPL and distributed with Clozure CL as the
+;;;   file "LICENSE".  The LLGPL consists of a preamble and the LGPL,
+;;;   which is distributed with Clozure CL as the file "LGPL".  Where these
+;;;   conflict, the preamble takes precedence.  
+;;;
+;;;   Clozure CL is referenced in the preamble as the "LIBRARY."
+;;;
+;;;   The LLGPL is also available online at
+;;;   http://opensource.franz.com/preamble.html
+
+(defsystem "ccldoc"
+    :depends-on ("alexandria"
+		 "split-sequence"
+		 "s-xml")
+    :serial t
+    :components ((:file "package")
+		 (:file "utils")
+		 (:file "representation")
+		 (:file "toplevel")
+		 (:file "syntax")
+		 (:file "output-docbook")
+		 (:file "output-tracwiki")
+		 (:file "output-ccldoc")))
---
+++ trunk/source/package.lisp
@@ -0,0 +1,77 @@
+(defpackage ccldoc
+  (:import-from :alexandria
+                #:when-let* #:when-let #:if-let
+                #:starts-with-subseq)
+  (:import-from :split-sequence #:split-sequence)
+  (:import-from :ccl
+                #:pkg-arg #:no-such-package
+                #:report-condition
+                #:definition-base-name #:definition-type-instance #:method-def-parameters
+                #:non-nil-symbolp #:setf-function-name-p
+                #:parse-macro-1
+                #:record-arglist
+                #:cheap-eval-in-environment
+                #:read-recording-source #:*loading-toplevel-location* #:*nx-source-note-map*
+                #:block-like #:progn-print #:*print-right-margin*
+                #:*show-condition-context*)
+  ;;; Syntax.  Don't really need to export these, but might as well collect them in one place
+  (:export
+   ;; operators
+   :include-file
+   :defsection :index-section :glossary-section
+   :block :code-block :glossentry :definition
+   :table :row :item :para
+   :clause :index :link :markup :ref :quote
+   ;; macros
+   :document :chapter :section
+   :variable :function :macro :type :refdef :term
+   :emphasis :system :sample :param :code
+   :lbrace :rbrace)
+  ;;; API
+  (:export
+   #:load-document
+   #:output-docbook
+   #:output-tracwiki
+   #:output-ccldoc
+   #:def-expander
+   #:def-definition-type)
+  ;;; DOM
+  (:export
+   ;; concrete classes
+   #:document
+   #:index-section
+   #:glossary-section
+   #:section
+   #:xref #:xref-target #:xref-default-body
+   #:indexed-clause
+   #:link #:link-url
+   #:glossentry
+   #:definition #:definition-display-name #:definition-summary #:definition-signature
+   #:table
+   #:row
+   #:listing #:listing-type
+   #:code-block
+   #:markup #:markup-type
+   #:block
+   #:para
+   #:term-item
+   #:item
+   #:docerror
+   ;; abstract classes
+   #:clause-with-items #:clause-items
+   #:clause-with-body #:clause-body
+   #:clause-with-title #:clause-title
+   #:clause-with-term #:clause-term
+   #:named-clause #:clause-name #:clause-external-id
+   #:clause-object
+     #:clause-parent
+     #:clause-document
+     #:section-level
+     #:clause-text
+   ;; type
+   #:clause
+   ;; dspec (definition clause-name)
+   #:dspec
+   #:dspec-type
+   #:dspec-name
+   #:dspec-type-name))
